3edce3bb37fc82c39a26f1c99a73b2bcb0f4fd39
amalshehu <amalshehu@gmail.com>
2017-11-09 13:42:19 +0530

    Add methods and dispatch actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { Observable } from 'rxjs/Rx';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
+interface AppState {
+  displayMessage: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  displayMessage$: Observable<string>;
+  constructor(private store: Store<AppState>) {
+   this.displayMessage$ = this.store.select('displayMessage');
+  }
+  leftMessage() {
+    this.store.dispatch({type: 'LEFT'});
+  }
+  rightMessage() {
+    this.store.dispatch({type: 'RIGHT'});
+  }
 }
