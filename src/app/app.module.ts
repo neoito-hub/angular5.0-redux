88ddc3d364f5a06dfcff5fba0fb9e832ca9622f5
amalshehu <amalshehu@gmail.com>
2017-11-09 13:42:50 +0530

    Import store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+import { StoreModule } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { AppReducer } from './reducers/app.reducer';
+import { PostReducer } from './reducers/post.reducer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({displayMessage: AppReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
